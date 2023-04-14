c3eb4cca4b8126a0ddc485eace36b51472e37ae9
NCInside <nchristian02@student.ciputra.ac.id>
2023-04-14 09:48:47 +0700

    Chapter 1 - Handling User Input - Section 6

---
+++ 0706012110045-Nicholas CI-AFL 3/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteButton.swift
+//  0706012110045-Nicholas CI-AFL 3
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
+
