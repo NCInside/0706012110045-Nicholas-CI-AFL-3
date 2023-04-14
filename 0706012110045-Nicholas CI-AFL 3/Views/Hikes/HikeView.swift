6eb0ef7ddf9f4460001257f3ab6b078fbe74b676
NCInside <nchristian02@student.ciputra.ac.id>
2023-04-14 10:16:46 +0700

    Chapter 2 - Animating Views and Transitions - Section 2

--- 0706012110045-Nicholas CI-AFL 3/Views/Hikes/HikeView.swift
+++ 0706012110045-Nicholas CI-AFL 3/Views/Hikes/HikeView.swift
@@ -32,7 +32,10 @@ struct HikeView: View {
                         .labelStyle(.iconOnly)
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
+
+                        .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
+
                 }
             }
 
@@ -42,6 +45,7 @@ struct HikeView: View {
         }
     }
 }
+
 
 struct HikeView_Previews: PreviewProvider {
     static var previews: some View {
