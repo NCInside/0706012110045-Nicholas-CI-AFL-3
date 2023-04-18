f6c3d2e5c8975fda2b117fc490031967cc231831
NCInside <nchristian02@student.ciputra.ac.id>
2023-04-18 07:31:20 +0700

    Chapter 3 - Composing Complex Interfaces - Section 1

---
+++ 0706012110045-Nicholas CI-AFL 3/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  0706012110045-Nicholas CI-AFL 3
+//
+//  Created by MacBook Pro on 18/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
