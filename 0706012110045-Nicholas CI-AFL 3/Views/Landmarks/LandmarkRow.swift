7ffa792ccc6d98c5740786bbfef7b4e7728ecbb1
NCInside <nchristian02@student.ciputra.ac.id>
2023-04-27 19:25:10 +0700

    Chapter 4 - Creating a macOS App - Section 3

--- 0706012110045-Nicholas CI-AFL 3/Views/Landmarks/LandmarkRow.swift
+++ 0706012110045-Nicholas CI-AFL 3/Views/Landmarks/LandmarkRow.swift
@@ -10,21 +10,32 @@ import SwiftUI
 // The struct to represent a row consisting of landmark image and name in a list view
 struct LandmarkRow: View {
     var landmark: Landmark
-    
+
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
-            Text(landmark.name)
-            
+                .cornerRadius(5)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .bold()
+                #if !os(watchOS)
+                Text(landmark.park)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                #endif
+            }
+
             Spacer()
-            
+
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
+                    .imageScale(.medium)
                     .foregroundColor(.yellow)
             }
         }
+        .padding(.vertical, 4)
     }
 }
 
